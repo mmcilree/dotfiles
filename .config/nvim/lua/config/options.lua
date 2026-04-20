44ea959b03a4e618764d9c26c28cc908e1991a10
Mcilree Matthew <matthew.j.mcilree@gmail.com>
2026-04-20 15:58:34 +0100

    nvim: better paste.

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -9,14 +9,21 @@ vim.opt.relativenumber = true
 vim.diagnostic.config({ virtual_text = true })
 
 -- So I can copy over ssh
+local function paste()
+	return {
+		vim.fn.split(vim.fn.getreg(""), "\n"),
+		vim.fn.getregtype(""),
+	}
+end
+
 vim.g.clipboard = {
 	name = "OSC 52",
 	copy = {
 		["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-		["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+		["*"] = require("vim.ui.clipboard.osc52").copy("+"),
 	},
-	paste = {
-		["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-		["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+	pase = {
+		["+"] = paste,
+		["*"] = paste,
 	},
 }
