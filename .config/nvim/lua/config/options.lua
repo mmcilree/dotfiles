f294b848413d315df1c9e7f985582aa8694c00d9
Mcilree Matthew <matthew.j.mcilree@gmail.com>
2026-04-20 14:24:54 +0100

    nvim: Add option to copy over ssh

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -7,3 +7,16 @@ vim.opt.relativenumber = true
 
 -- Inline diagnostics
 vim.diagnostic.config({ virtual_text = true })
+
+-- So I can copy over ssh
+vim.g.clipboard = {
+	name = "OSC 52",
+	copy = {
+		["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+		["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+	},
+	paste = {
+		["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+		["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+	},
+}
