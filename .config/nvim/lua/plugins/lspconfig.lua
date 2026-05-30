f268857c040ea89db58dae561f859db824bda225
mmcilree <matthew.j.mcilree@gmail.com>
2026-05-30 11:00:14 +0100

    nvim: downgrade basedpyright typechecking mode.

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -24,6 +24,13 @@ return {
 				},
 			},
 		})
+		vim.lsp.config("basedpyright", {
+			settings = {
+				basedpyright = {
+					typeCheckingMode = "basic",
+				},
+			},
+		})
 		vim.lsp.enable({ "lua_ls" })
 		vim.lsp.enable({ "ltex_ls" })
 		vim.lsp.enable({ "basedpyright" })
