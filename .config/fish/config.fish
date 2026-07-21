b6200495e3885c08fd0c6fbe50b1e814340a32a8
mmcilree <matthew.j.mcilree@gmail.com>
2026-07-21 17:06:58 +0100

    fish: config.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,3 +13,12 @@ end
 function fataepyc
 	ssh -t mmcilree@ssh1.dcs.gla.ac.uk ssh -t fataepyc-$argv[1] 
 end
+
+
+# BEGIN opam configuration
+# This is useful if you're using opam as it adds:
+#   - the correct directories to the PATH
+#   - auto-completion for the opam binary
+# This section can be safely removed at any time if needed.
+test -r '/Users/matthewmcilree/.opam/opam-init/init.fish' && source '/Users/matthewmcilree/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
+# END opam configuration
